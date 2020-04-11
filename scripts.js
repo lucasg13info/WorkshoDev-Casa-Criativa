a1a33e4d7b29896f78903af40f13ed05c9d06d8b
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-11 13:35:30 -0300

    Refatorando o JavaScript

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Fazer o Com amor Rocketseat apareça e desapareça
+    document
+        .querySelector("button.fat")
+        .addEventListener("click", function(){
+            document
+                .querySelector("#footer")
+                .classList
+                .toggle("hide")
+        })

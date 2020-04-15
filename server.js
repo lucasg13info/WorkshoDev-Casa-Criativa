1135c8b59bb8555ff754b883b1083df8d10a7e7a
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-14 23:33:06 -0300

    Testando funções e retornando parametros

--- server.js
+++ server.js
@@ -5,7 +5,13 @@
 const mensagem ="Oi, como vai?"
 const number = 2.5 
 
+function soma(numero1, numero2){
+        console.log(numero1 + numero2)
+       
+}
 
-
-console.log(mensagem)
-console.log(number)
+soma(10, 20)
+soma(5, 5 )
+soma(6, 7)
+soma(8, 9)
+soma(0, 4)

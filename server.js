be1b7a0ff4dc4e84ead56a4302fa4f329c893f90
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-14 23:27:00 -0300

    Testando variaveis

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 // Vamos falar de coisas básicas da programação em JavaScript
+
+// Variaveis  ??
+
+const mensagem ="Oi, como vai?"
+const number = 2.5 
+
+
+
+console.log(mensagem)
+console.log(number)

4ccb4a0e9b82fbd629d761d06fe480d8b92941ee
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-15 00:59:59 -0300

    Ajustando o index.html e ideias.html para funcionar no node

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 const express = require("express")
 const server = express()
 
+//configurar arquivos estáticos (css, script, imagens)
+server.use(express.static("public"))
+
+
 // Criei uma rota /
 //e capturo o pedido do cliente para responder
 server.get("/", function(req, res){
-    console.log("cheguei")
+    console.log("Estou pronto")
     return res.sendFile(__dirname +"/index.html")
 })
 
+server.get("/ideias", function(req, res){
+    console.log("Estou pronto")
+    return res.sendFile(__dirname +"/ideias.html")
+})
 
 // liguei meu servidor na porta 3000
 server.listen(3000)

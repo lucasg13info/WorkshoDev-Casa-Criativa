772b46a9eaf0a8c9023a6f7687873107451af883
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-18 14:46:23 -0300

    Corrigindo bug do banco

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 // Configurando banco de dados
 const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database( './ws.db')
+const db = new sqlite3.Database('./ws.db')
 
 
 db.serialize(function(){
     // CRIAR A TABELA
     db.run(`
     CREATE TABLE IF NOT EXISTS ideas(
-        id INTEGER PRIMARY KEY AUTOINCREMET,
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
         image TEXT,
         title TEXT,
         category TEXT

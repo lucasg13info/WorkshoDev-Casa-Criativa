7e2c35b78170babc2d9722bbccaa7eae9b22b869
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-20 19:21:57 -0300

    Corrigindo erro do banco

--- db.js
+++ db.js
@@ -1,55 +1,45 @@
-// Configurando banco de dados
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./ws.db')
 
-
 db.serialize(function(){
-    // CRIAR A TABELA
+  
+    //CRIAR A TABELA
     db.run(`
-    CREATE TABLE IF NOT EXISTS ideas(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image TEXT,
-        title TEXT,
-        category TEXT
-        description TEXT,
-        link TEXT
-    );
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT, 
+            title TEXT, 
+            category TEXT,
+            description TEXT,
+            link TEXT     
+        );    
     `)
 
     //INSERIR DADOS NA TABELA
     const query = `
-    INSERT INTO ideas(
-        image,
-        title,
-        category,
-        description,
-        link
-    ) VALUES (?,?,?,?,?);
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES(?,?,?,?,?);
     `
-        const values = [
-            "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-            "Curso de Programação",
-            "Estudo",
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-            "https://rocketseat.com.br"
-       ]
 
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Curso de Programação",
+        "Estudo",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam modi voluptates fugit in iusto nam blanditiis facilis incidunt aliquam",
+        "https://rocketseat.com.br"
+    ]
 
-        //db.run(query, values, function(err) {
-        //    if (err) return console.log(err)
-       //
-        //    console.log(this)
-            
-        //} ) /**Vai rodas as variaves definidas acima */
+    db.run(query, values, function(err) {
+        if (err) return console.log(err)
+           console.log(this)
+    })
 
     //CONSULTAR DADOS NA TABELA
 
-    db.all(`SELECT * FROM ideas`, function(err, rows){
-        if (err) return console.log(err)
-
-        console.log(rows)
-
-    })
-
-    // DELETAR UM DADO NA TABELA
+    //DELETAR UM DADO NA TABELA
 })

c3dbf3ae66f4cc4cd753a8847ae15d65c8b96de7
Lucas  Rodrigues Estefano <42548142+lucasg13info@users.noreply.github.com>
2020-04-23 20:56:27 -0300

    Zerando o banco

--- db.js
+++ db.js
@@ -4,41 +4,41 @@ const db = new sqlite3.Database('./ws.db')
 db.serialize(function(){
   
     //CRIAR A TABELA
-   // db.run(`
-    //    CREATE TABLE IF NOT EXISTS ideas(
-     //       id INTEGER PRIMARY KEY AUTOINCREMENT,
-     //       image TEXT, 
-      //      title TEXT, 
-      //      category TEXT,
-        //    description TEXT,
-        //    link TEXT     
-       // );    
-   // `)
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT, 
+            title TEXT, 
+            category TEXT,
+            description TEXT,
+            link TEXT     
+        );    
+    `)
 
     //INSERIR DADOS NA TABELA
-    //const query = `
-    //    INSERT INTO ideas(
-    //        image,
-      //      title,
-        //    category,
-          //  description,
-          //  link
-        //) VALUES(?,?,?,?,?);
-   // `
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES(?,?,?,?,?);
+    `
 
-    //const values = [
-    //    "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    //    "Curso de Programação",
-    //    "Estudo",
-     //   "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam modi voluptates fugit in iusto nam blanditiis facilis incidunt aliquam",
-    //    "https://rocketseat.com.br"
-   // ]
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Curso de Programação",
+        "Estudo",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam modi voluptates fugit in iusto nam blanditiis facilis incidunt aliquam",
+        "https://rocketseat.com.br"
+    ]
 
-    //db.run(query, values, function(err) {
-    //  if (err) return console.log(err)
+    db.run(query, values, function(err) {
+      if (err) return console.log(err)
 
-     //    console.log(this)
-    //})
+         console.log(this)
+    })
 
    /*
     //DELETAR UM DADO NA TABELA
